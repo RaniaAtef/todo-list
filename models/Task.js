8c790fa665fae61a9a76e66117cbf8f28b922d7b
Rania atef <raniaatef718@gmail.com>
2026-02-25 23:32:01 +0200

    fix:layout

--- models/Task.js
+++ models/Task.js
@@ -7,12 +7,10 @@ const TaskSchema = new mongoose.Schema({
   column: { type: String, required: true },
   priority: { type: String, enum: ['low', 'medium', 'high'], default: 'low' },
 }, {
-  // Don't use MongoDB's default _id for our task id, 
-  // we are managing our own 'id' field
+
   timestamps: true, 
   toJSON: {
     transform: function(doc, ret) {
-      // Always return our custom 'id' field, remove Mongo's _id and __v
       delete ret._id;
       delete ret.__v;
       delete ret.createdAt;
@@ -22,7 +20,6 @@ const TaskSchema = new mongoose.Schema({
   }
 });
 
-// Prevent model recompilation in dev hot-reload
 const Task = mongoose.models.Task || mongoose.model('Task', TaskSchema);
 
 export default Task;
